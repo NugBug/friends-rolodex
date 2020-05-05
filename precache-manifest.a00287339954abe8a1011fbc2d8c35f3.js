5b0cb8e0e62e1801e1f4b6d0add50777e662df12
joshtanguay <joshtanguay@gmail.com>
2020-05-05 11:25:27 -0700

    Updates

---
+++ precache-manifest.a00287339954abe8a1011fbc2d8c35f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf398ff7b818e6791b6fce5a770981b",
+    "url": "/friends-rolodex/index.html"
+  },
+  {
+    "revision": "ece5fd9d4f464acdbba3",
+    "url": "/friends-rolodex/static/css/main.47ae5909.chunk.css"
+  },
+  {
+    "revision": "1582648ad15a56347620",
+    "url": "/friends-rolodex/static/js/2.435b072a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/friends-rolodex/static/js/2.435b072a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ece5fd9d4f464acdbba3",
+    "url": "/friends-rolodex/static/js/main.653e2b93.chunk.js"
+  },
+  {
+    "revision": "78aef81e65780c3d5e65",
+    "url": "/friends-rolodex/static/js/runtime-main.2afed907.js"
+  }
+]);
