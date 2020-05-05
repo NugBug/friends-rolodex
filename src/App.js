9704a7fff9854ef9a64bba12151fd86297ed01ff
joshtanguay <joshtanguay@gmail.com>
2020-05-05 11:18:22 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.components.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      friends: [],
+      searchField: "",
+    };
+  }
+
+  async componentDidMount() {
+    const users = await (
+      await fetch("https://jsonplaceholder.typicode.com/users")
+    ).json();
+    this.setState({ friends: users });
+  }
+
+  render() {
+    const { friends, searchField } = this.state;
+    const filteredFriends = friends.filter((friend) =>
+      friend.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1>Friend Rolodex</h1>
+        <SearchBox
+          placeholder="search friends"
+          handleChange={(e) => this.setState({ searchField: e.target.value })}
+        />
+        <CardList friends={filteredFriends} />
+      </div>
+    );
+  }
 }
 
 export default App;
